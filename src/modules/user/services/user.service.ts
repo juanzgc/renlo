9d564fc036b94cf4973268d9439c4cf064ee3968
Juan Zapata Gomez <juanzgc@bu.edu>
2022-08-18 17:27:39 -0500

    Add User Service

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,13 +1,17 @@
 import { EntityManager } from 'typeorm';
 import { FindConfig } from '@medusajs/medusa/dist/types/common';
+import { MedusaError } from 'medusa-core-utils';
 import { EventBusService, User } from '@medusajs/medusa';
 import { UserService as MedusaUserService } from '@medusajs/medusa';
+import { validateId, buildQuery } from '@medusajs/medusa/dist/utils';
 import {
   EntityEventType,
   MedusaEventHandlerParams,
   OnMedusaEntityEvent,
   Service
 } from 'medusa-extender';
+
+// Extended
 import UserRepository from '../repositories/user.repository';
 import UserSubscriber from '../subscribers/user.subscriber';
 
@@ -30,6 +34,7 @@ export default class UserService extends MedusaUserService {
     this.eventBus = container.eventBusService;
 
     // Only works when this is not commented. Middleware doesn't seem to work.
+    // @TODO: Confirm, bug suspicion, that Subscribers are only working when attached to the Service. (Won't work if only attached in the middleware)
     UserSubscriber.attachTo(this.manager.connection);
   }
 
@@ -42,8 +47,37 @@ export default class UserService extends MedusaUserService {
     return event;
   }
 
-  async retrieve(userId: string, config?: FindConfig<User>): Promise<User> {
-    console.log('retrieve');
-    return await super.retrieve(userId, config);
+  /**
+   * Retrieve user and extend to directly query the user being retrieved/
+   *
+   * @TODO: Return extended User type instead of the Core User type.
+   *
+   * @param userId
+   * @param config
+   * @returns
+   */
+  public async retrieve(
+    userId: string,
+    config?: FindConfig<User>
+  ): Promise<User> {
+    const userRepo = this.manager.getCustomRepository(this.userRepository);
+    const validatedId = validateId(userId);
+
+    const selector = {
+      id: validatedId
+    };
+
+    const query = buildQuery(selector, config);
+
+    const user = await userRepo.findOne(query);
+
+    if (!user) {
+      throw new MedusaError(
+        MedusaError.Types.NOT_FOUND,
+        `User with id: ${userId} was not found`
+      );
+    }
+
+    return user as User;
   }
 }
